7df3d9732214c0925473e49045082d5947e4739e
Micaela Vargas <48757979+micavargas130@users.noreply.github.com>
2020-10-25 19:48:23 -0300

    Add files via upload

---
+++ bin/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- You can use this file to load seed data into the database using SQL statements
+
+
+insert into SOCIO (NOMBRE, APELLIDO, EMAIL, DNI) values ('Edu', 'Gaite', 'egaite@gmail.com', '259543337');
+insert into PELICULA (TITULO, SINOPSIS, ANIO, GENERO) values ('deTest', 'de Test', '2020', 0);
