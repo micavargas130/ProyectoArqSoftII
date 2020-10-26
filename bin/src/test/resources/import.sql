a3d14e6b14c01f853a984f94db480e5aee6e7760
micavargas130 <mica.vargas2000@gmail.com>
2020-10-26 17:35:31 -0300

    cambios

--- bin/src/test/resources/import.sql
+++
@@ -1,7 +0,0 @@
--- You can use this file to load seed data into the database using SQL statements
-insert into STATE (name) values ('Salta');
-insert into STATE (name) values ('Córdoba');
-
-insert into USER (name, last_name, email) values ('Jose Maria', 'Quiroga', 'jmquiroga@hotmail.com');
-
-insert into ADDRESS (address_type, number, street, state_id, user_id) values (3, 2015, 'Independencia', 2, 1);
